f4960fc525b560ebc0e3c7c83581ab795f925459
Rajiv <1998irajeev@gmail.com>
2021-05-30 18:31:19 +0530

    husky setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="bg-white text-red-500">
-          Edit <code>src/App.tsx</code> and save to reload.
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p className="bg-white text-red-500">
+              Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+             className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
