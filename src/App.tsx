5bf6d10947d1c5214e87df878a87d5c9b176be31
Rajiv <1998irajeev@gmail.com>
2021-05-30 18:36:27 +0530

    husky testing

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,16 @@ import logo from "./logo.svg";
 
 import "./App.css";
 
-
-
-
 function App() {
   return (
     <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p className="bg-white text-red-500">
-              Edit <code>src/App.tsx</code> and save to reload.
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p className="bg-white text-red-500">
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-             className="App-link"
+          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
