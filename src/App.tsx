ca2063ab8a3362817696a558af3f5dcd573a73b4
Rajiv <1998irajeev@gmail.com>
2021-05-30 19:03:07 +0530

    husky test

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+/**
+ * APp Component
+ * @return {React.ReactNode}
+ */
 function App() {
   return (
     <div className="App">
